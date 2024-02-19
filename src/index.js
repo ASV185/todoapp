9c4e1c8ca38adea5aa128f9850c8611cb6733165
Amanda Ventura <74805696+ASV185@users.noreply.github.com>
2024-02-18 23:29:18 -0700

    added "" instead of ' ' and added event instead of e

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-const form = document.querySelector('#submitForm');
-const input = document.querySelector('#list-input');
-const list = document.querySelector('#newUL');
+const form = document.querySelector("#submitForm");
+const input = document.querySelector("#list-input");
+const list = document.querySelector("#newUL");
 
-form.addEventListener('submit', function(e){
-    e.preventDefault(); 
-    const choreName = input.value;
-    const newLI = document.createElement('LI');
-    newLI.innerText = choreName;
-    list.append(newLI);
-    input.value ="";
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const choreName = input.value;
+  const newLI = document.createElement("LI");
+  newLI.innerText = choreName;
+  list.append(newLI);
+  input.value = "";
 });
 
-list.addEventListener('click', function(e){
-    e.target.nodeName === 'LI'&&
-    e.target.remove();
-})
+list.addEventListener("click", function (event) {
+  event.target.nodeName === "LI" && event.target.remove();
+});
